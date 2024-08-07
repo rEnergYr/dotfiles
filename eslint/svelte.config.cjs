ac13b40cc59a7f141e8de3998db2b5ea5b5f5828
Elric <elric.gumbau@icloud.com>
2024-08-07 15:27:17 +0200

    🔧 update svelte.config.cjs

--- eslint/svelte.config.cjs
+++ eslint/svelte.config.cjs
@@ -7,12 +7,12 @@ module.exports = {
     },
     "ignorePatterns": ["node_modules", ".eslintrc.cjs", "*.json", "build", ".svelte-kit"],
     "extends": [
+      "plugin:@typescript-eslint/recommended",
       "airbnb-base",
       "plugin:svelte/recommended",
       "plugin:prettier/recommended"
     ],
     "plugins": [
-      "@typescript-eslint",
       "prettier"
     ],
     "rules": {}
